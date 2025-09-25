a131e23091ade379b030bef7ddc93e1e1a84c010
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-26 01:57:48 +0330

    create register serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -8,3 +8,28 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ["id", "username", "email", "first_name", "last_name", "role"]
         read_only_fields = ("id", "role")
+
+
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField(
+        max_length=150,
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message="This username is already registered.",
+            )
+        ],
+    )
+    email = serializers.EmailField(
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message="This email has already been registered.",
+            )
+        ]
+    )
+    password = serializers.CharField(write_only=True, style={"input_type": "password"})
+    password2 = serializers.CharField(write_only=True, style={"input_type": "password"})
+    role = serializers.ChoiceField(
+        choices=User.ROLE_CHOICES, default=User.ROLE_CUSTOMER
+    )
