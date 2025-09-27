a2560066b999ea251000ab2acbddc43c6b5f4f0c
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-27 16:49:18 +0330

    create Category model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
