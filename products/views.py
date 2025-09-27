381fed382f8f4810d4a307be427be63f691c858d
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-27 22:47:07 +0330

    complete views

--- products/views.py
+++ products/views.py
@@ -1,18 +1,24 @@
-from rest_framework.generics import ListAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework.viewsets import ModelViewSet
 from products.models import Category, Product
 from products.permissions import IsSellerOrReadOnly
 from products.serializers import CategorySerializer, ProductSerializer
 
 
-class CategoryListAPIView(ListAPIView):
+class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [AllowAny]
+
+    def get_permissions(self):
+        if self.request.method in ["GET", "HEAD", "OPTIONS"]:
+            return [AllowAny()]
+        return [IsAdminUser()]
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.select_related("category", "seller").all()
     serializer_class = ProductSerializer
     permission_classes = [IsSellerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(seller=self.request.user)
