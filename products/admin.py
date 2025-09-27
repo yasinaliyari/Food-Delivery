9dc5f04fd18cd297d3967b3192116a26f1c9d54c
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-27 21:50:17 +0330

    register Category

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from products.models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
