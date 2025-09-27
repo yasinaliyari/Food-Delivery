9f10ef1f90ef2d8299b92c33b8198d9920b77466
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-27 16:56:39 +0330

    create Product model

--- products/models.py
+++ products/models.py
@@ -13,3 +13,21 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="products")
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="products"
+    )
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.name
