92d729a9e3d03d7e7f00277ae9dae6574f162b51
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-27 17:20:52 +0330

    create CategoryListAPIView

--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import AllowAny
+from products.models import Category
+from products.serializers import CategorySerializer
 
-# Create your views here.
+
+class CategoryListAPIView(ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [AllowAny]
