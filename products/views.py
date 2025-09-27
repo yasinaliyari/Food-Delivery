5c0f1b2b25aa1aac4727c72a239fa913c40038b1
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-27 17:23:24 +0330

    create ProductViewSet

--- products/views.py
+++ products/views.py
@@ -1,10 +1,18 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny
-from products.models import Category
-from products.serializers import CategorySerializer
+from rest_framework.viewsets import ModelViewSet
+from products.models import Category, Product
+from products.permissions import IsSellerOrReadOnly
+from products.serializers import CategorySerializer, ProductSerializer
 
 
 class CategoryListAPIView(ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [AllowAny]
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.select_related("category", "seller").all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsSellerOrReadOnly]
