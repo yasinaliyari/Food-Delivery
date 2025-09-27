40e2a3ec508ec71fae808c0160a79f1c942fb5c8
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-27 17:04:19 +0330

    create ProductSerializer

--- products/serializers.py
+++ products/serializers.py
@@ -1,8 +1,30 @@
 from rest_framework import serializers
-from products.models import Category
+from products.models import Category, Product
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ["id", "name", "slug"]
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(), source="category", write_only=True
+    )
+
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "name",
+            "description",
+            "price",
+            "stock",
+            "category",
+            "category_id",
+            "seller",
+            "created_at",
+        ]
+        read_only_fields = ["id", "seller", "created_at"]
