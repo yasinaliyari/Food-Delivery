73b6dc78e8667403238909aad01d3ef4da15ac7b
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-27 21:50:31 +0330

    register Product

--- products/admin.py
+++ products/admin.py
@@ -6,3 +6,10 @@ from products.models import Category, Product
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "slug")
     prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "stock", "category", "seller", "created_at")
+    list_filter = ("category", "seller")
+    search_fields = ("name", "description")
