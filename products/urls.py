11b29631012020559312d4e579d0a743078907d1
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-27 22:47:30 +0330

    add categories urls

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from products.views import ProductViewSet, CategoryListAPIView
+from products.views import CategoryViewSet, ProductViewSet
 
 router = DefaultRouter()
+router.register(r"categories", CategoryViewSet, basename="category")
 router.register(r"products", ProductViewSet, basename="product")
 
 urlpatterns = [
-    path("categories/", CategoryListAPIView.as_view(), name="category-list"),
     path("", include(router.urls)),
 ]
