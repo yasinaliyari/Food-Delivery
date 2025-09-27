ed26035f77023fa757088aa5defe890dd0aece87
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-27 17:28:43 +0330

    add products urls

--- food_delivery/urls.py
+++ food_delivery/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/accounts/", include("accounts.urls")),
+    path("api/products/", include("products.urls")),
 ]
