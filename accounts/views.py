dcf86ad5fad9a6eef96263a6a8cbaa30af3d545d
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-27 15:47:29 +0330

    create MyProfile view

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
-from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import AllowAny
-from accounts.serializers import RegisterSerializer
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from accounts.serializers import RegisterSerializer, UserSerializer
 
 User = get_user_model()
 
@@ -9,3 +9,11 @@ User = get_user_model()
 class RegisterAPIView(CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
+
+
+class MeAPIView(RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
