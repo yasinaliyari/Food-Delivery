abd02d6aa796857abb200deb1f622b9d03e674a8
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-28 13:36:28 +0330

    register Order

--- orders/admin.py
+++ orders/admin.py
@@ -8,3 +8,11 @@ class OrderItemInline(admin.TabularInline):
     readonly_fields = [
         "price",
     ]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "status", "total_price", "created_at")
+    list_filter = ("status", "created_at")
+    search_fields = ("user__username", "id")
+    inlines = [OrderItemInline]
