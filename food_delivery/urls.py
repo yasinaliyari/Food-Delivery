b1153d237607799fa2c8381ea4ec45f5c5a48285
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-28 15:20:44 +0330

    create orders urls

--- food_delivery/urls.py
+++ food_delivery/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/accounts/", include("accounts.urls")),
     path("api/products/", include("products.urls")),
+    path("api/orders/", include("orders.urls")),
 ]
---
+++ orders/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from orders.views import OrderViewSet
+
+router = DefaultRouter()
+router.register(r"orders", OrderViewSet, basename="order")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
