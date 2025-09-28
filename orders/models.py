3b5bfac91e67011199e45cffe84a2b1075972960
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-28 13:32:02 +0330

    create OrderItem model

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.validators import MinValueValidator
 from django.db import models
 from products.models import Product
 
@@ -30,3 +31,19 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order #{self.pk} - {self.user} - {self.status}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(
+        Product, on_delete=models.PROTECT, related_name="order_items"
+    )
+    quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.product.name} x{self.quantity}"
+
+    @property
+    def line_total(self):
+        return self.price * self.quantity
