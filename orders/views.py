048ac8cb9e674cfe4f07765edf20775b6d374aed
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-29 16:43:31 +0330

    add set_status, cancel function

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,5 @@
 from rest_framework import viewsets, mixins
 from rest_framework.decorators import action
-from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.throttling import ScopedRateThrottle
@@ -36,12 +35,7 @@ class OrderViewSet(
     def get_queryset(self):
         qs = super().get_queryset()
         user = self.request.user
-        if user.is_staff:
-            return qs
-        return qs.filter(user=user)
-
-    def perform_create(self, serializer):
-        serializer.save()
+        return qs if user.is_staff else qs.filter(user=user)
 
     def create(self, request, *args, **kwargs):
         write_serializer = self.get_serializer(
@@ -60,11 +54,42 @@ class OrderViewSet(
         detail=True,
         methods=["patch"],
         url_path="status",
-        permission_classes=[IsAdminUser],
+        permission_classes=[IsAuthenticated],
     )
     def set_status(self, request, pk=None):
         order = self.get_object()
-        serializer = OrderStatusUpdateSerializer(order, data=request.data, partial=True)
+        if not (
+            request.user.is_staff or getattr(request.user, "role", None) == "seller"
+        ):
+            return Response(
+                {"detail": "Only admin or seller van change status"},
+                status=http_status.HTTP_403_FORBIDDEN,
+            )
+        serializer = OrderStatusUpdateSerializer(
+            order, data=request.data, partial=True, context={"request": request}
+        )
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        return Response(OrderSerializer(order).data, status=http_status.HTTP_200_OK)
+
+    @action(
+        detail=True,
+        methods=["post"],
+        url_path="cancel",
+        permission_classes=[IsAuthenticated],
+    )
+    def cancel(self, request, pk=None):
+        order = self.get_object()
+        if order.user != request.user and not request.user.is_staff:
+            return Response(
+                {"detail": "You can only cancel your own order"},
+                status=http_status.HTTP_403_FORBIDDEN,
+            )
+        if order.status != Order.STATUS_PENDING:
+            return Response(
+                {"detail": "Only pending orders can be canceled"},
+                status=http_status.HTTP_400_BAD_REQUEST,
+            )
+        order.status = Order.STATUS_CANCELED
+        order.save(update_fields=["status"])
         return Response(OrderSerializer(order).data, status=http_status.HTTP_200_OK)
