a252d066f1b4433ce5422e564659def31c5b8dd0
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-29 18:42:36 +0330

    create Review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,27 @@
+from django.conf import settings
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="reviews"
+    )
+    rating = models.PositiveSmallIntegerField()
+    comment = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "product"], name="unique_user_product_review"
+            )
+        ]
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.user} => {self.product} ({self.rating})"
