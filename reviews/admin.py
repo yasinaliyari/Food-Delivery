7760e001a7a99c087b8c1e84f9f3098c50c5fe1c
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-29 18:43:17 +0330

    register Review model

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from reviews.models import Review
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "product", "rating", "created_at")
+    list_filter = ("rating", "created_at")
+    search_fields = ("user__username ", "product__name", "comment")
