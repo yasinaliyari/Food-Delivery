e882961606b24163162174c430f7952756472788
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-29 16:42:21 +0330

    add IsSeller permission

--- orders/permissions.py
+++ orders/permissions.py
@@ -1,8 +1,16 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
 
-class IsOwnerOrAdmin(BasePermission):
+class IsOwnerOrAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.user and request.user.is_staff:
             return True
         return getattr(obj, "user", None) == request.user
+
+
+class IsSeller(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+            and getattr(request.user, "role", None) == "seller"
+        )
